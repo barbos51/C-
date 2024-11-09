81dbf5cf60f6158d14988c4f821867ece5e88f28
barbos51 <ivanovicbarbos51@gmail.com>
2024-11-09 11:45:00 +0200

    Pr_3

---
+++ WebApplication1/WebApplication1/Models/Dto/GameDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models.Dto
+{
+    public class GameDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebApplication1.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+    }
+}
