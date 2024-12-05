bb24dbf1fb465a185ce7a0180831c06b43cf7fc1
barbos51 <ivanovicbarbos51@gmail.com>
2024-12-05 08:42:57 +0200

    додано час, дату, десеріалізація для часу у секундах і дати у форматі mm-dd-yy

--- ConsoleApp/JsonSerializerExample/Program.cs
+++ ConsoleApp/JsonSerializerExample/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 using ConsoleApp;
 
 var person = new Person
 {
     Name = "skif",
     Age = 28,
-    Email = "skif@example.com"
+    Email = "skif@example.com",
+    Time = 30,
+    Date = new DateTime(2024, 11, 10, 15, 30, 0)
 };
 
 var jsonString = JsonSerializer.Serialize(person);
@@ -18,8 +21,22 @@ if (deserializedPerson == null)
 
 Console.WriteLine($"Name: {deserializedPerson.Name}, " +
                   $"Age: {deserializedPerson.Age}, " +
-                  $"Email: {deserializedPerson.Email}");
+                  $"Email: {deserializedPerson.Email}, " +
+                  $"Time: {deserializedPerson.Time*60}, " +
+                  $"Date: {deserializedPerson.Date.ToString("MM-dd-yy")}");
 
+public class DateTimeConverter : JsonConverter<DateTime>
+{
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateTime.Parse(reader.GetString()!);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString("MM-dd-yy"));
+    }
+}
 
 
 namespace ConsoleApp
@@ -29,5 +46,7 @@ namespace ConsoleApp
         public string Name { get; init; } = null!;
         public int Age { get; init; }
         public string Email { get; init; } = null!;
+        public int Time { get; init; }
+        public DateTime Date { get; init; }
     }
 }
